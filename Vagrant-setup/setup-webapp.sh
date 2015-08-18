cd27f39fc60eda91721995f4fffbe254a694d843
Daniel Meyer <danielmeyer@danielrmeyer.emsi>
2015-08-18 15:42:24 -0700

    minor changes to provisioning and checking in migration script

--- Vagrant-setup/setup-webapp.sh
+++ Vagrant-setup/setup-webapp.sh
@@ -7,6 +7,6 @@ sudo apt-get -y install python-dev
 sudo pip install -r /vagrant/requirements.txt
 
 cd /vagrant/rest_demo
-python manage.py makemigrations snippets
+#python manage.py makemigrations snippets
 python manage.py migrate
 python manage.py runserver 0.0.0.0:80
